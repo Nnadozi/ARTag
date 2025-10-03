c1a3a082b5ffe4c744206a14f185c7be7d7775e1
Nnadozi <chikaosro@gmail.com>
2025-10-03 18:52:16 -0400

    RNUI

---
+++ app/(main)/Post.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+  
+const Post = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Post</Text>
+    </View>
+  )
+}
+
+export default Post
+
+const styles = StyleSheet.create({})
--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -4,8 +4,9 @@ export default function MainLayout() {
   //Try out native tabs later
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-        <Tabs.Screen name="index"  />
+        <Tabs.Screen name="index" options={{ href: null }} />
         <Tabs.Screen name="Feed" />
+        <Tabs.Screen name="Post" />
     </Tabs>
   )
 }
